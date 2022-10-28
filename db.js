41d8996a6c529329b312798fc14ea23e98df232b
Ziiron1 <guilherme_profissional@yahoo.com.br>
2022-10-27 23:49:35 -0300

    Server's Running

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
-const pratoentrada = require("./data/pratoentrada");
+const pratos = require("./data/pratos");
 const bebidas = require("./data/bebidas");
 const sobremesa = require("./data/sobremesa");
-const categoria = require("./data/categoria");
+const categorias = require("./data/categorias");
 
 module.exports = {
-  pratoentrada,
+  pratos,
   bebidas,
   sobremesa,
-  categoria
+  categorias
 }
 
 
